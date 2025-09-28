b590cf7eaf41051dd662be39b1835060496201f5
giusegit <giuseppe.cicala@hotmail.it>
2025-09-28 12:08:11 +0200

    Added InputAction and Starter Content in DefaultGame config

--- Source/TicTacToe_UE56/Private/TTT_PlayerController.cpp
+++ Source/TicTacToe_UE56/Private/TTT_PlayerController.cpp
@@ -3,3 +3,18 @@
 
 #include "TTT_PlayerController.h"
 
+ATTT_PlayerController::ATTT_PlayerController()
+{
+}
+
+void ATTT_PlayerController::ClickOnGrid()
+{
+}
+
+void ATTT_PlayerController::BeginPlay()
+{
+}
+
+void ATTT_PlayerController::SetupInputComponent()
+{
+}
--- Source/TicTacToe_UE56/Public/TTT_PlayerController.h
+++ Source/TicTacToe_UE56/Public/TTT_PlayerController.h
@@ -4,6 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/PlayerController.h"
+#include "TTT_HumanPlayer.h"
+#include "InputActionValue.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 #include "TTT_PlayerController.generated.h"
 
 /**
@@ -13,5 +17,21 @@ UCLASS()
 class TICTACTOE_UE56_API ATTT_PlayerController : public APlayerController
 {
 	GENERATED_BODY()
+
+public:
+	ATTT_PlayerController();
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputMappingContext* TTTContext;
+
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* ClickAction;
+
+	void ClickOnGrid();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 	
 };
